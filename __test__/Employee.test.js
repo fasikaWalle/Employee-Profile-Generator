8bb1000266258fc1e9f32561414d43e49f80fa89
fasikaWalle <fasikabini12@gmail.com>
2021-02-10 16:43:06 -0600

    Employee class added

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,28 +1,28 @@
-const Employee = requre("../lib/Employee");
+const Employee = require("../lib/Employee");
 
 test("Check employee object", () => {
   const employee = new Employee(1, "fasika", "fasika@gmail.com");
   expect(employee.id).toEqual(expect.any(Number));
-  expect(employee.id).tobeDefined();
+  expect(employee.id).toBeDefined();
   expect(employee.name).toEqual(expect.any(String));
-  expect(employee.name).tobeDefined();
+  expect(employee.name).toBeDefined();
   expect(employee.email).toEqual(expect.any(String));
-  expect(employee.email).tobeDefined();
+  expect(employee.email).toBeDefined();
 });
 
 test("Gets employee id", () => {
   const employee = new Employee(1, "fasika", "fasika@gmail.com");
-  expect(employee.getId()).toBe(employe.id);
+  expect(employee.getId()).toBe(employee.id);
 });
 
 test("Gets employee name", () => {
   const employee = new Employee(1, "fasika", "fasika@gmail.com");
-  expect(employee.getName()).toBe(employe.name);
+  expect(employee.getName()).toBe(employee.name);
 });
 
 test("Gets employee email", () => {
   const employee = new Employee(1, "fasika", "fasika@gmail.com");
-  expect(employee.getEmail()).toBe(employe.email);
+  expect(employee.getEmail()).toBe(employee.email);
 });
 
 test("Gets employee role", () => {
