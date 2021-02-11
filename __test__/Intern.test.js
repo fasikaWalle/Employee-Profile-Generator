abe26cf2ece2d6ee43b78b8b97d049fefe882ea1
fasikaWalle <fasikabini12@gmail.com>
2021-02-10 20:18:03 -0600

    Intern test added

---
+++ __test__/Intern.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('@jest/globals')
+const Intern=require('../lib/Intern')
+test('Checks intern object',()=>{
+    const intern=new Intern(3,'fasika','fasika@gmail.com','Austin university of Managment')
+    expect(intern.school).toEqual(expect.any(String))
+    expect(intern.school).toBeDefined()
+})
+test("Gets intern school", () => {
+    const intern=new Intern(3,'fasika','fasika@gmail.com','Austin university of Managment')
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school))
+  });
+
+test("Gets intern role", () => {
+    const intern=new Intern(3,'fasika','fasika@gmail.com','Austin university of Managment')
+    expect(intern.getRole()).toBe(intern.constructor.name);
+  });
